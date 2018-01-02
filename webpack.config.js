3cf14d495eb7eca944d6059350b3226f207ba604
fxf <932294542@qq.com>
2018-01-02 20:00:26 +0800

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,27 @@
 * @Author: fxf
 * @Date:   2017-12-10 17:19:23
 * @Last Modified by:   fxf
-* @Last Modified time: 2017-12-10 22:52:55
+* @Last Modified time: 2018-01-02 19:40:58
 */
 var webpack           = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//环境变量配置，dev / online
+var WEBPACK_ENV       = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV);
+
+//获取html-webpack-plugin参数的方法
+var getHtmlConfig = function(name){
+    return{
+            template : './src/view/'+name+'.html',
+            filename : 'view/'+name+'.html',
+            inject   : true,
+            hash     : true,
+            chunks   : ['common',name]
+    };
+};
+//webpack config
 var config = {
      entry:{
         'common' : ['./src/page/common/index.js'],
@@ -14,6 +31,7 @@ var config = {
      } ,
      output: {
          path: './dist',
+         publicPath: '/dist',
          filename: 'js/[name].js'
      },
      externals : {
@@ -21,18 +39,26 @@ var config = {
      },
      module: {
         loaders : [
-            {
-                test : /\.css$/, loader : ExtractTextPlugin.extract("style-loader","css-loader")
-            }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
+            { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' }
         ]
      },
      plugins:[
+        //独立通用模块到js/base.js
         new webpack.optimize.CommonsChunkPlugin({
             name : 'common',//通用模块
             filename : 'js/base.js'
         }),
-        new ExtractTextPlugin('css/[name].css')
+        //把css单独打包到文件里
+        new ExtractTextPlugin('css/[name].css'),
+        //html模板的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
      ]
  };
+
+ if ('dev' === WEBPACK_ENV) {
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+ }
 
  module.exports = config;
