6c35bac6553c2c499aa7d6a862f148dd5309baff
fxf <932294542@qq.com>
2018-01-07 16:45:50 +0800

    common部分

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,12 +2,39 @@
 * @Author: fxf
 * @Date:   2017-12-10 17:12:38
 * @Last Modified by:   fxf
-* @Last Modified time: 2017-12-10 22:34:23
+* @Last Modified time: 2018-01-07 16:22:20
 */
 
 // console.log('hello index');
 // var $$ = require('jquery');
 // $$('body').html('index hrllosdhsgh --');
 
-require('./index.css');
-require('../module.js');
+/*require('./index.css');
+require('../module.js');*/
+
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
+var navSide = require('page/common/nav-side/index.js');
+var _mm = require('util/mm.js');
+
+navSide.init({
+    name : 'user-pass-update'
+});
+
+/*_mm.request({
+    url: '/product/list.do?keyword=1',
+    success: function(res){
+        console.log(res);
+    },
+    error: function(errMsg){
+        console.log(errMsg);
+    }
+});*/
+
+/*console.log(_mm.getUrlParam('test'));*/
+
+/*var html = '<div>{{data}}</div>';
+var data = {
+    data : 'test'
+}
+console.log(_mm.renderHtml(html,data));*/
