d22a760e1580f612144cecfcda34a0d484c73bc3
fxf <932294542@qq.com>
2018-03-01 17:59:46 +0800

    payment

---
+++ src/service/payment-service.js
@@ -0,0 +1,35 @@
+/*
+* @Author: fxf
+* @Date:   2018-03-01 16:53:29
+* @Last Modified by:   fxf
+* @Last Modified time: 2018-03-01 17:31:47
+*/
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _payment = {
+    // 获取支付信息
+    getPaymentInfo : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/pay.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    //获取订单状态
+    getPaymentStatus : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/query_order_pay_status.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _payment;
