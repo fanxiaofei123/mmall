066dccc9b8ca2986e54d235c1fd432e93afb9b97
fxf <932294542@qq.com>
2018-04-19 21:55:02 +0800

    xiugai lujing

---
+++ src/page/about/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: fxf
+* @Date:   2018-03-01 22:15:57
+* @Last Modified by:   fxf
+* @Last Modified time: 2018-03-01 22:17:10
+*/
+'use strict';
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
